77e435159e2f6fd1296c352d8c1cc880fd4c6b7f
c0sm0thecoder <agazadekamal2004@gmail.com>
2025-03-01 15:39:22 +0400

    gemini config

---
+++ src/config/llm.ts
@@ -0,0 +1,27 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { getEnvironmentVariables } from "./environment";
+
+let genAI: GoogleGenerativeAI;
+const MODEL_NAME = 'gemini-1.5-pro';
+
+export function initGemini(): GoogleGenerativeAI {
+    const env = getEnvironmentVariables();
+    genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
+    return genAI;
+}
+
+export function getGeminiModel() {
+    if (!genAI) {
+        initGemini();
+    }
+    return genAI.getGenerativeModel({ model: MODEL_NAME })
+}
+
+export async function generateEmbeddings(text: string): Promise<number[]> {
+    if (!genAI) {
+        initGemini();
+    }
+    const embeddingModel = genAI.getGenerativeModel({model: 'text-embedding-004'});
+    const result = await embeddingModel.embedContent(text);
+    return result.embedding.values;
+}
